a689a29b056932893fe858b6dd3c8d52089abb25
jujub316 <juliana.bain@gmail.com>
2017-12-02 16:02:52 -0500

    graphing

--- graphing.ml
+++ graphing.ml
@@ -1,5 +1,8 @@
 open GMain
 open GdkKeysyms
+open GDraw
+open GObj
+open GtkObject
 
 let locale = GtkMain.Main.init ()
 
@@ -20,13 +23,43 @@ let main () =
   factory#add_item "Quit" ~key:_Q ~callback: Main.quit;
 
   (* Button *)
-  let button = GButton.button ~label:"Push me!"
+  let button = GButton.button ~label:"+"
                               ~packing:vbox#add () in
-  button#connect#clicked ~callback: (fun () -> prerr_endline "Ouch!");
+  button#connect#clicked ~callback: (fun () -> prerr_endline "zoom in");
+
+  let button = GButton.button ~label:"-"
+                              ~packing:vbox#add () in
+  button#connect#clicked ~callback: (fun () -> prerr_endline "zoom out");
+
+  (*  (* Create the drawing area. *)
+  let da = GMisc.drawing_area ~packing:vbox#add () in
+  let drawable = lazy (new GDraw.drawable da#misc#window) in
+
+  object (self)
+(* inherit widget vbox#as_widget *)
+inherit gtk_object obj
+
+    initializer
+      ignore(da#event#connect#expose
+               ~callback:(fun _ -> self#repaint (); false));
+      ignore(adjustment#connect#value_changed
+               ~callback:(fun _ -> self#repaint ()))
+
+  method repaint ()=
+    let drawable = Lazy.force drawable in
+    let (width, height) = drawable#size in
+  drawable#set_background `WHITE;
+      drawable#line ~x:40 ~y:(height-40) ~x:(width-40) ~y:(height-40); *)
+
+  let array = Array.init 100 (fun _ -> Random.int 10) in
+    (* Create a graph in the main area. *)
+    let graph = new graph font ~packing:vbox#add array in
+    graph#set_title "Random data";
+
 
   (* Display the windows and enter Gtk+ main loop *)
   window#add_accel_group accel_group;
   window#show ();
   Main.main ()
 
-let () = main ()
+  let () = main ()
